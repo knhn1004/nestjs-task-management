16ca63bc3a0b9a38304ff9e0d912accf629b18dc
chiahong <chiahong@pop-os.localdomain>
2021-05-06 15:35:58 +0800

    remove some trash

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -17,7 +17,6 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
-// import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
